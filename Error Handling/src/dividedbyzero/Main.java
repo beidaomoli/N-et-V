690fd6bcbc99b40e3da4e585b5053a56894cebf3
beidaomoli <2521186394@qq.com>
2024-03-20 17:16:22 +0800

    NSS

--- Error Handling/src/dividedbyzero/Main.java
+++ Error Handling/src/dividedbyzero/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+	try {
 		System.out.println(3/0);
-	}
-
+		}
+	catch(Exception e) {
+	System.out.printf("Caught runtime exception:%s%n",e);
+}
+}
 }
