db324d0f0871823908eadd7afa2892b7d981ac79
noahhoa <971877209@qq.com>
2024-03-20 19:47:22 +0800

    Exception specified

--- Error Handling/src/dividedbyzero/Main.java
+++ Error Handling/src/dividedbyzero/Main.java
@@ -1,18 +1,15 @@
 package dividedbyzero;
 
-public class Main {
+public class main {
 
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO 自动生成的方法存根
-	try {
-=======
-		// TODO 
->>>>>>> 73ea1fda04a991b0491bdcb8568200d79f3e3674
-		System.out.println(3/0);
-		}
-	catch(Exception e) {
-	System.out.printf("Caught runtime exception:%s%n",e);
-}
-}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+        }
+    }
 }
